5e3b78a4fa5fcaadccfae5b22452b80e6aab1cd0
Nicocube <nicolas.lochet@gmail.com>
2011-12-19 22:02:50 +0100

    blah

--- main.js
+++ main.js
@@ -1,19 +1,13 @@
 var https = require('https');
 var fs = require('fs');
+var parse = require('url').parse;
+var server = require('./server');
 
-var options = {
-	key: fs.readFileSync('server-key.pem'),
-	cert: fs.readFileSync('server-cert.pem')
-};
-
-var count = 0;
-
-https.createServer(options, function (req, res) {
-	count++;
-	console.log("call #"+count);
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	fs.readFile('./static/index.html', function (err, data) {
-		if (err) throw err;
-		res.end(data);
-	});
-}).listen(8000);
+https.createServer(
+	{
+		key: fs.readFileSync('server-key.pem'),
+		cert: fs.readFileSync('server-cert.pem')
+	},
+	server.callback(parse,fs)
+).
+listen(8000);
