941349d335c78c6ef8f7218ab0ecdd4103b2edfa
Nicocube <nicolas.lochet@gmail.com>
2012-04-02 04:15:27 +0200

    Great spring cleaning.

---
+++ index.js
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2012 Nicolas Lochet Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ *      
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+// simple export to move on with
+
+module.exports = {
+	filter: require('./lib/filter.js').inject,
+	dispatch: require('./lib/dispatch.js').inject,
+	http_status: require('./lib/http-status.js').inject.status,
+	rest: require('./lib/rest.js').inject,
+	auth: require('./lib/auth.js').inject,
+	static: require('./lib/static.js').inject,
+	db: require('./lib/db.js').inject,
+	biz: require('./lib/biz.js').inject,
+}
