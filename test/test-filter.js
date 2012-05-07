5e21338996620476fb45fce3eac61a975dacc218
Nicocube <nicolas.lochet@gmail.com>
2012-05-07 17:41:10 +0200

    remove meaningless semicolon

--- test/test-filter.js
+++ test/test-filter.js
@@ -9,141 +9,141 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-var counter = require('../lib/counter.js');
-var filter_module = require('../lib/filter.js').inject('url');
-var filter = filter_module.root_filter;
-var not = filter_module.not;
+var counter = require('../lib/counter.js')
+var filter_module = require('../lib/filter.js').inject('url')
+var filter = filter_module.root_filter
+var not = filter_module.not
 
 exports['should always accept filter true'] = function (test) {
-	var f = filter(true);
-	test.ok(f(), 'could filter nothing');
-	test.ok(f('/'), 'could not filter /');
-	test.ok(f('plop'), 'could not filter plop');
-	test.ok(f('+qf%??df'), 'could not filter +qf%??df');
-	test.done();
+	var f = filter(true)
+	test.ok(f(), 'could filter nothing')
+	test.ok(f('/'), 'could not filter /')
+	test.ok(f('plop'), 'could not filter plop')
+	test.ok(f('+qf%??df'), 'could not filter +qf%??df')
+	test.done()
 }
 
 exports['should use function to filter req.url'] = function (test) {
-	var c = counter.build();
+	var c = counter.build()
 	var x = {}
-	var fun = function(o) {c.inc();return o;}
-	var y = filter(fun)({url:x});
-	test.strictEqual(x,y);
-	test.equal(c.check(),1);
-	test.done();
+	var fun = function(o) {c.inc();return o}
+	var y = filter(fun)({url:x})
+	test.strictEqual(x,y)
+	test.equal(c.check(),1)
+	test.done()
 }
 
 exports['should use string filter as equal req.url'] = function (test) {
-	var f = filter('/a');
-	test.throws(function () {f()});
-	test.ok(! f({url:'/'}));
-	test.ok(! f({url:'plop'}));
-	test.ok(! f({url:'/plop'}));
-	test.ok(f({url:'/a'}));
-	test.ok(!f({url:'/abba'}));
-	test.done();
+	var f = filter('/a')
+	test.throws(function () {f()})
+	test.ok(! f({url:'/'}))
+	test.ok(! f({url:'plop'}))
+	test.ok(! f({url:'/plop'}))
+	test.ok(f({url:'/a'}))
+	test.ok(!f({url:'/abba'}))
+	test.done()
 }
 
 exports['should use regex filter to match req.url'] = function (test) {
-	var f = filter(/^\/a/);
-	test.throws(function () {f()});
-	test.ok(! f('plop'));
-	test.ok(! f({url:'/'}));
-	test.ok(! f({url:'plop'}));
-	test.ok(! f({url:'/plop'}));
-	test.ok(f({url:'/a'}));
-	test.ok(f({url:'/abba'}));
-	test.done();
+	var f = filter(/^\/a/)
+	test.throws(function () {f()})
+	test.ok(! f('plop'))
+	test.ok(! f({url:'/'}))
+	test.ok(! f({url:'plop'}))
+	test.ok(! f({url:'/plop'}))
+	test.ok(f({url:'/a'}))
+	test.ok(f({url:'/abba'}))
+	test.done()
 }
 
 exports['should fail if filter is integer'] = function (test) {
-	test.throws(function () {filter(42)});
-	test.done();
+	test.throws(function () {filter(42)})
+	test.done()
 }
 
 exports['should accept if any filter is ok on req.url'] = function (test) {
-	var c_call = counter.build();
+	var c_call = counter.build()
 
 	test.ok(filter([
-		function() {c_call.inc();return false;},
-		function() {c_call.inc();return true;},
+		function() {c_call.inc();return false},
+		function() {c_call.inc();return true},
 		function() {
-			test.ok(false,"Should never be called");
+			test.ok(false,"Should never be called")
 		}
-	])({}));
+	])({}))
 	
-	test.equals(c_call.check(),2);
+	test.equals(c_call.check(),2)
 	
 	test.ok(filter([
-		function() {return false;},
+		function() {return false},
 		'^pl',
 		true,
 		/op$/
-	])({url:'plop'}));	
+	])({url:'plop'}))	
 
 	test.ok(filter([
 		{b:'yy'},
 		{a:'x'},
 		{b:'y'}
-	])({a:'xx', b:'yy'}));
+	])({a:'xx', b:'yy'}))
 
 	test.ok(!filter([
 		false,
-		function() {return false;},
+		function() {return false},
 		/u/
-	])({url:'xx'}));
+	])({url:'xx'}))
 
-	test.throws(function () {filter([true, 42])});
+	test.throws(function () {filter([true, 42])})
 	
-	test.done();
+	test.done()
 }
 
 exports['should tempt to match req if filter object is {x: <regex> }'] = function (test) {
-	var f = filter({'x' : /\.html$/, 'y': 'PLOP'});
-	test.ok(! f({x:'/'}));
-	test.ok(! f({y:'plop'}));
-	test.ok(! f({x: 'xxx', y:'PLOP'}));
-	test.ok(! f({x: 'test/plip.html', y:'xxx'}));
-	test.ok(f({x : '/index.html', y:'PLOP'}));
-	test.ok(f({x : '/index.html', y:'PLOP', z: 'AAAA'}));
-	test.done();
+	var f = filter({'x' : /\.html$/, 'y': 'PLOP'})
+	test.ok(! f({x:'/'}))
+	test.ok(! f({y:'plop'}))
+	test.ok(! f({x: 'xxx', y:'PLOP'}))
+	test.ok(! f({x: 'test/plip.html', y:'xxx'}))
+	test.ok(f({x : '/index.html', y:'PLOP'}))
+	test.ok(f({x : '/index.html', y:'PLOP', z: 'AAAA'}))
+	test.done()
 }
 
 exports['should tempt complex stuff'] = function (test) {
 	var f = filter([
 		{'x': /\.html$/, 'h': {'a' : function(x){ return x.indexOf('x')!=-1 },'b': ['o','u','e']}},
 		{'u':'plop'}
-	]);
+	])
 
-	test.ok(! f({x:'/'}));
-	test.ok(! f({y:'plop'}));
-	test.ok(! f({x: 'xxx', y:'PLOP'}));
-	test.ok(! f({x: 'test/plip.html', y:'xxx'}));
-	test.ok(! f({x:'plop.html',h:{a:'ooooxooo',b:'i'}}));
-	test.ok(f({u:'plop'}));
-	test.ok(f({x:'plop.html',h:{a:'ooooxooo',b:'u'}}));
+	test.ok(! f({x:'/'}))
+	test.ok(! f({y:'plop'}))
+	test.ok(! f({x: 'xxx', y:'PLOP'}))
+	test.ok(! f({x: 'test/plip.html', y:'xxx'}))
+	test.ok(! f({x:'plop.html',h:{a:'ooooxooo',b:'i'}}))
+	test.ok(f({u:'plop'}))
+	test.ok(f({x:'plop.html',h:{a:'ooooxooo',b:'u'}}))
 	
-	test.done();
+	test.done()
 }
 
 exports['should negate filter'] = function (test) {
 	
-	test.ok(not(false)(undefined));
+	test.ok(not(false)(undefined))
 
-	test.ok(not(/x/)('j'));
-	test.ok(! not(/x/)({url:'x'}));
+	test.ok(not(/x/)('j'))
+	test.ok(! not(/x/)({url:'x'}))
 
-	test.ok(not('u')('j'));
-	test.ok(! not('u')({url:'u'}));
+	test.ok(not('u')('j'))
+	test.ok(! not('u')({url:'u'}))
 
-	test.ok(not(function(a) {return a=='g'})('j'));
-	test.ok(! not(function(a) {return a=='g'})({url:'g'}));
+	test.ok(not(function(a) {return a=='g'})('j'))
+	test.ok(! not(function(a) {return a=='g'})({url:'g'}))
 
-	test.ok(not(['g','v'])('j'));
-	test.ok(! not(['g','v'])({url:'g'}));
+	test.ok(not(['g','v'])('j'))
+	test.ok(! not(['g','v'])({url:'g'}))
 
-	test.ok(not({a:'u'})('j'));
-	test.ok(! not({a:'u'})({a:'u'}));
+	test.ok(not({a:'u'})('j'))
+	test.ok(! not({a:'u'})({a:'u'}))
 	
-	test.done();
+	test.done()
 }
