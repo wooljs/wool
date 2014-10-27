8ac1a3a19c7b7b85a3192f1697c9b0043bbfb874
Nicocube <nicolas.lochet@gmail.com>
2014-10-27 19:46:43 +0100

    add some specs

--- spec/validator.spec.js
+++ spec/validator.spec.js
@@ -10,11 +10,11 @@
  */
 
 var
-Validator = require( __dirname + '/../lib/validator.js')()
+V = require( __dirname + '/../lib/validator.js')()
 
 describe("build a Validator with", function() {
     it("empty definition validator is truthy", function() {
-        validator = Validator.build()
+        validator = V()
         expect(validator()).toEqual(true)
         expect(validator(undefined)).toEqual(true)
         expect(validator({})).toEqual(true)
@@ -22,8 +22,8 @@ describe("build a Validator with", function() {
         expect(/\{\s*return true\s*\}$/.test(validator.toString())).toEqual(true)
     });
     
-    it("definition of type undefined validator is truthy", function() {
-        validator = Validator.build(undefined)
+    it("definition of type undefined is truthy", function() {
+        validator = V(undefined)
         expect(validator()).toEqual(true)
         expect(validator(undefined)).toEqual(true)
         expect(validator({})).toEqual(true)
@@ -31,9 +31,121 @@ describe("build a Validator with", function() {
         expect(/\{\s*return true\s*\}$/.test(validator.toString())).toEqual(true)
     });
     
-    it("definition of type function validator is truthy", function() {
+    it("definition of type function is indentity", function() {
         function f() {}
-        validator = Validator.build(f)
+        validator = V(f)
         expect(validator).toEqual(f)
+    });
+    
+    describe("definition of type object with a key and value ", function() {
+        it("is true", function() {
+            var def = {
+                a_key: true
+            }
+            validator = V(def)
+            expect(validator({a_key: true})).toEqual(true)
+            expect(validator({a_key: 1})).toEqual(true)
+            expect(validator({a_key: 'plop'})).toEqual(true)
+            expect(validator({a_key: { other_key : 'paf'}})).toEqual(true)
+            expect(validator({a_key: []})).toEqual(true)
+            expect(validator({ other_key : 'paf'})).toEqual(false)
+        });
+        
+        it("of type string is of form '=val'", function() {
+            var def = {
+                a_key: '=plop'
+            }
+            validator = V(def)
+            expect(validator({a_key: true})).toEqual(false)
+            expect(validator({a_key: 1})).toEqual(false)
+            expect(validator({a_key: 'plop'})).toEqual(true)
+            expect(validator({a_key: { other_key : 'paf'}})).toEqual(false)
+            expect(validator({a_key: []})).toEqual(false)
+            expect(validator({ other_key : 'paf'})).toEqual(false)
+        });
+        
+        it("of type string is 'boolean'", function() {
+            var def = {
+                a_key: 'boolean',
+            }
+            validator = V(def)
+            expect(validator({a_key: true})).toEqual(true)
+            expect(validator({a_key: 1})).toEqual(false)
+            expect(validator({a_key: 'plop'})).toEqual(false)
+            expect(validator({a_key: { other_key : 'paf'}})).toEqual(false)
+            expect(validator({a_key: []})).toEqual(false)
+            expect(validator({ other_key : 'paf'})).toEqual(false)
+        });
+        
+        it("of type string is 'number'", function() {
+            var def = {
+                a_key: 'number',
+            }
+            validator = V(def)
+            expect(validator({a_key: true})).toEqual(false)
+            expect(validator({a_key: 1})).toEqual(true)
+            expect(validator({a_key: 'plop'})).toEqual(false)
+            expect(validator({a_key: { other_key : 'paf'}})).toEqual(false)
+            expect(validator({a_key: []})).toEqual(false)
+            expect(validator({ other_key : 'paf'})).toEqual(false)
+        });
+        
+        it("of type string is 'string'", function() {
+            var def = {
+                a_key: 'string',
+            }
+            validator = V(def)
+            expect(validator({a_key: true})).toEqual(false)
+            expect(validator({a_key: 1})).toEqual(false)
+            expect(validator({a_key: 'plop'})).toEqual(true)
+            expect(validator({a_key: { other_key : 'paf'}})).toEqual(false)
+            expect(validator({a_key: []})).toEqual(false)
+            expect(validator({ other_key : 'paf'})).toEqual(false)
+        });
+        
+        it("of type string is 'object'", function() {
+            var def = {
+                a_key: 'object',
+            }
+            validator = V(def)
+            expect(validator({a_key: true})).toEqual(false)
+            expect(validator({a_key: 1})).toEqual(false)
+            expect(validator({a_key: 'plop'})).toEqual(false)
+            expect(validator({a_key: { other_key : 'paf'}})).toEqual(true)
+            expect(validator({a_key: []})).toEqual(true)
+            expect(validator({ other_key : 'paf'})).toEqual(false)
+        });
+        
+        it("of type string is 'array'", function() {
+            var def = {
+                a_key: 'array',
+            }
+            validator = V(def)
+            expect(validator({a_key: true})).toEqual(false)
+            expect(validator({a_key: 1})).toEqual(false)
+            expect(validator({a_key: 'plop'})).toEqual(false)
+            expect(validator({a_key: { other_key : 'paf'}})).toEqual(false)
+            expect(validator({a_key: []})).toEqual(true)
+            expect(validator({ other_key : 'paf'})).toEqual(false)
+        });
+        
+        it("of type RegExp", function() {
+            var def = {
+                a_key: /op/,
+            }
+            validator = V(def)
+            expect(validator({a_key: true})).toEqual(false)
+            expect(validator({a_key: 1})).toEqual(false)
+            expect(validator({a_key: { other_key : 'paf'}})).toEqual(false)
+            expect(validator({a_key: []})).toEqual(false)
+            expect(validator({ other_key : 'paf'})).toEqual(false)
+            
+            expect(validator({a_key: 'plip'})).toEqual(false)
+            
+            expect(validator({a_key: 'plop'})).toEqual(true)
+            expect(validator({a_key: 'operand'})).toEqual(true)
+            expect(validator({a_key: 'pope'})).toEqual(true)
+        });
+        
     });
 });
