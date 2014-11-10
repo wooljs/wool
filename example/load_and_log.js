f90a7adf05fcea7d66a5c400cc8d6957fce6dc7d
Nicocube <nicolas.lochet@gmail.com>
2014-11-10 19:02:52 +0100

    fix a example

--- example/load_and_log.js
+++ example/load_and_log.js
@@ -3,7 +3,7 @@ var load = require( __dirname + '/../lib/load.js')
 
 var loader = load(require('fs'), require('event-stream'))
     
-loader(__dirname + '/../spec/test.db', function(obj, i) {
+loader(__dirname + '/test_load.db', function(obj, i) {
     console.log(i, obj)
 }, function() {
     console.log('done')    
