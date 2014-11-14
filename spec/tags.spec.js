f6eac0378185d0a4a0aa180e95fc15cde61be9c6
Nicocube <nicolas.lochet@gmail.com>
2014-11-14 13:46:39 +0100

    idea for tags component

---
+++ spec/tags.spec.js
@@ -0,0 +1,22 @@
+
+/*
+
+Store.<data>() : read data value
+Store.<data>.listen(this) : this.notify will be called when data change (passing delta ?)
+
+wool.prepare(function (User) { 
+    return div('.content', h1(User.name), 
+        ul(each(User.tasks,    // is each necessary ?
+            li(checkbox($('checked')), $('name'))
+        ))
+      )
+})
+
+each([1,2..], function(t) { return li(...) }) // bof
+
+each([1,2,..], 'e', p($('e.field')))
+
+a({click: evq(..
+
+
+*/
