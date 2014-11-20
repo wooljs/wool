981afe5584e551682d16160bf24032aa6dcf6503
Nicocube <nicolas.lochet@gmail.com>
2014-11-20 17:25:18 +0100

    move tags code to proper lib

--- example/tags_experiment.js
+++ example/tags_experiment.js
@@ -65,19 +65,65 @@ function $(k) {
 }
 
 
-
-console.log (
-    div(h1("Hello ", $('name'), "!"),
-        ul(each([1,2,3],
-            li('plip:', $())
-        )),
-        div(
-            each({i: [1,2,3]},
-                div('i:', $('i')),
-                each({j: [1,2,3]},
-                    div('j:', $('i'), '*', $('j'))
-                )
+var template = div(h1("Hello ", $('name'), "!"),
+    ul(each([1,2,3],
+        li('plip:', $())
+    )),
+    div(
+        each({i: [1,2,3]},
+            div('i:', $('i')),
+            each({j: [1,2,3]},
+                div('j:', $('i'), '*', $('j'))
             )
         )
-    )({name: 'Plop'})(formatter)
+    )
 )
+
+console.log (
+    template({name: 'Plop'})(formatter)
+)
+
+function bench(n) {
+    console.time(n+' loop')
+    for (var i = 0 ; i < n ; i+=1) {
+        template({name: 'Plop'})(formatter)
+    }
+    console.timeEnd(n+' loop')
+}
+
+bench(100)
+bench(1000)
+//bench(1000000)
+
+var arr = []
+for (var i = 0; i<10000; i+=1) {
+    arr.push(i)
+}
+
+function sqr(a) { return a * a }
+
+function bench_map(n) {
+    console.time(n+' loop map')
+    for (var i = 0 ; i < n ; i+=1) {
+        var t = arr.map(sqr)
+    }
+    console.timeEnd(n+' loop map')
+    console.log(t.length)
+}
+
+function bench_for(n) {
+    console.time(n+' loop for')
+    var t
+    for (var i = 0 ; i < n ; i+=1) {
+        t = []
+        for (var a = 0, l = arr.length; a < l; a+=1) {
+            var v = arr[a]
+            t.push(sqr(v))
+        }
+    }
+    console.timeEnd(n+' loop for')
+    console.log(t.length)
+}
+
+bench_map(10000)
+bench_for(10000)
--- spec/tags.spec.js
+++ spec/tags.spec.js
@@ -1,4 +1,40 @@
+/*
+ * Copyright 2014 Nicolas Lochet Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ *      
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
 
+var
+tags = require( __dirname + '/../lib/tags.js')()
+
+describe("template testing", function() {
+    "use strict"
+    
+    it("simple template", function() {
+        var template = tags(function() {
+            return div(h1("Hello ", $('name'), "!"),
+                ul(each([1,2,3],
+                    li('plip:', $())
+                )),
+                div(
+                    each({i: [1,2,3]},
+                        div('i:', $('i')),
+                        each({j: [1,2,3]},
+                            div('j:', $('i'), '*', $('j'))
+                        )
+                    )
+                )
+            )
+        })
+        var res = template({name: 'Plop'})        
+        expect(res).toEqual('<div><h1>Hello Plop!</h1><ul><li>plip:1</li><li>plip:2</li><li>plip:3</li></ul><div><div>i:1</div><div>j:1*1</div><div>j:1*2</div><div>j:1*3</div><div>i:2</div><div>j:2*1</div><div>j:2*2</div><div>j:2*3</div><div>i:3</div><div>j:3*1</div><div>j:3*2</div><div>j:3*3</div></div></div>')
+    });
+})
 /*
 
 Store.<data>() : read data value
@@ -20,3 +56,5 @@ a({click: evq(..
 
 
 */
+
+    
