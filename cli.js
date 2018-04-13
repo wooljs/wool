0d51d677cdd2b4b4dc7628fb18c074d20c7a6589
Nicocube <nicolas.lochet@gmail.com>
2018-04-13 15:15:30 +0200

    fix cli.js

--- cli.js
+++ cli.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 /*
  * Copyright 2017 Nicolas Lochet Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. You may obtain a copy of the License at
