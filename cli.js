e9584083c9e2d4c619c293bfaf113b1030e7a828
Nicocube <nicolas.lochet@gmail.com>
2018-09-10 16:57:17 +0200

    small change in package.json + var > const

--- cli.js
+++ cli.js
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-var meow = require('meow')
+const meow = require('meow')
   , fs = require('fs')
   , cli = meow(`
 Usages:
